cb542c888d85149d8f8d0bd950b4debe0a78e44e
youhelin <youhelin@51creation.com>
2025-04-18 09:26:49 +0800

    fix: 把解析变成一个组件

--- eslint.config.js
+++ eslint.config.js
@@ -16,4 +16,9 @@ export default [
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
   skipFormatting,
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  },
 ]
